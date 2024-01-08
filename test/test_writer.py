ef2427f7940c71c9300b79c0c9afa9f6e5d8db59
Stefan Hagen <stefan@hagen.link>
2024-01-08 22:45:31 +0100

    Completed test coverage for writer
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- test/test_writer.py
+++ test/test_writer.py
@@ -10,22 +10,14 @@ def test_write_csaf_default(mocker):
     path = pathlib.Path(file_path).expanduser().resolve()
     options = {'ensure_ascii': False, 'indent': 2}
     mock_file = mocker.mock_open()
-    mocker.patch("builtins.open", mock_file)
+    mocker.patch('builtins.open', mock_file)
     payload = {'csaf': 42}
     expected_message = (logging.INFO, f'Successfully wrote {path}.')
     scoped_messages = writer.write_csaf(payload, file_path)
 
     mock_file.assert_called_once_with(path, 'wt', encoding=writer.ENCODING)
     mock_file().write.assert_has_calls(
-        [
-            call('{'),
-            call(f'\n{" " * options["indent"]}'),
-            call('"csaf"'),
-            call(': '),
-            call('42'),
-            call('\n'),
-            call('}')
-        ]
+        [call('{'), call(f'\n{" " * options["indent"]}'), call('"csaf"'), call(': '), call('42'), call('\n'), call('}')]
     )
     assert scoped_messages == [expected_message]
 
@@ -35,22 +27,14 @@ def test_write_csaf_compact(mocker):
     path = pathlib.Path(file_path).expanduser().resolve()
     options = {'ensure_ascii': False, 'indent': 0}
     mock_file = mocker.mock_open()
-    mocker.patch("builtins.open", mock_file)
+    mocker.patch('builtins.open', mock_file)
     payload = {'csaf': 42}
     expected_message = (logging.INFO, f'Successfully wrote {path}.')
     scoped_messages = writer.write_csaf(payload, file_path, options)
 
     mock_file.assert_called_once_with(path, 'wt', encoding=writer.ENCODING)
     mock_file().write.assert_has_calls(
-        [
-            call('{'),
-            call('\n'),
-            call('"csaf"'),
-            call(': '),
-            call('42'),
-            call('\n'),
-            call('}')
-        ]
+        [call('{'), call('\n'), call('"csaf"'), call(': '), call('42'), call('\n'), call('}')]
     )
     assert scoped_messages == [expected_message]
 
@@ -60,25 +44,17 @@ def test_write_csaf_bad_suffix(mocker):
     path = pathlib.Path(file_path).expanduser().resolve()
     options = {'ensure_ascii': False, 'indent': 0}
     mock_file = mocker.mock_open()
-    mocker.patch("builtins.open", mock_file)
+    mocker.patch('builtins.open', mock_file)
     payload = {'csaf': 42}
     expected_messages = [
         (logging.WARNING, f'Given output file {path} does not contain valid {writer.CSAF_FILE_SUFFIX} suffix.'),
-        (logging.INFO, f'Successfully wrote {path}.')
+        (logging.INFO, f'Successfully wrote {path}.'),
     ]
     scoped_messages = writer.write_csaf(payload, file_path, options)
 
     mock_file.assert_called_once_with(path, 'wt', encoding=writer.ENCODING)
     mock_file().write.assert_has_calls(
-        [
-            call('{'),
-            call('\n'),
-            call('"csaf"'),
-            call(': '),
-            call('42'),
-            call('\n'),
-            call('}')
-        ]
+        [call('{'), call('\n'), call('"csaf"'), call(': '), call('42'), call('\n'), call('}')]
     )
     assert scoped_messages == expected_messages
 
@@ -92,3 +68,33 @@ def test_write_csaf_real_folder_no_file():
     scoped_messages = writer.write_csaf(payload, file_path, options)
 
     assert scoped_messages == [expected_message]
+
+
+def test_write_csaf_create_folder(tmp_path):
+    base_dir = tmp_path / 'missing-folder'
+    path = base_dir / 'ok.json'
+    file_path = str(path)
+    payload = {'csaf': 42}
+    expected_message = (logging.INFO, f'Created output folder {base_dir}.')
+    scoped_messages = writer.write_csaf(payload, file_path)
+
+    assert expected_message in scoped_messages
+
+
+def test_write_csaf_overwrite_file(tmp_path):
+    base_dir = tmp_path / 'folder'
+    base_dir.mkdir()
+    path = base_dir / 'ok.json'
+    file_path = str(path)
+    payload = {'csaf': 42}
+    expected_message = (logging.INFO, f'Successfully wrote {path}.')
+    scoped_messages = writer.write_csaf(payload, file_path)
+    assert scoped_messages == [expected_message]
+
+    # overwrite file
+    expected_messages = [
+        (logging.WARNING, f'Output {path} already exists. Overwriting it.'),
+        expected_message,
+    ]
+    scoped_messages = writer.write_csaf(payload, file_path)
+    assert scoped_messages == expected_messages
