60e8b48e962b114300d0331c68e31b5c4a8dcc02
Stefan Hagen <stefan@hagen.link>
2024-07-20 16:26:22 +0200

    Added notes type parser
    - added notes type parser
    - added test for notes
    - added assert on document collation test
    - added error trait to protocol
    - removed elements from test fixtures
    
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- muuntaa/subtree.py
+++ muuntaa/subtree.py
@@ -8,9 +8,11 @@ RootType = lxml.objectify.ObjectifiedElement
 
 class Subtree(Protocol):
     tree: dict[str, Any] = None  # type: ignore
+    some_error: bool = False
 
     def __init__(self) -> None:
         self.tree = {}
+        self.some_error = False
 
     def always(self, root: RootType) -> None:
         pass
@@ -30,6 +32,9 @@ class Subtree(Protocol):
 
     def dump(self) -> dict[str, Any]:
         return self.tree
+
+    def has_errors(self) -> bool:
+        return self.some_error
 
 
 class DocumentLeafs(Subtree):
@@ -119,3 +124,52 @@ class Acknowledgments(Subtree):
                 record['urls'] = [url.text for url in urls]  # URLs can have more entries
 
             self.hook.append(record)
+
+
+class Notes(Subtree):
+    """Represent any Notes objects.
+
+    (
+        /cvrf:cvrfdoc/cvrf:DocumentNotes,
+      - /cvrf:cvrfdoc/vuln:Vulnerability[i+1]/vuln:Notes,
+    )
+    """
+
+    ENUM_CATEGORIES = {'description', 'details', 'faq', 'general', 'legal_disclaimer', 'other', 'summary'}
+    ENUM_MSG = ','.join(ENUM_CATEGORIES)
+
+    def __init__(self, lc_parent_code: str):  # TODO: unlitter me and push data upstream
+        super().__init__()
+        if lc_parent_code not in ('cvrf', 'vuln'):
+            raise KeyError('Notes can only be hosted by cvrf or vuln')
+        if lc_parent_code == 'cvrf':
+            if self.tree.get('document') is None:
+                self.tree['document'] = {}
+            if self.tree['document'].get('notes') is None:
+                self.tree['document']['notes'] = []
+            self.hook = self.tree['document']['notes']
+        else:
+            if self.tree.get('vulnerabilities') is None:
+                self.tree['vulnerabilities'] = {}
+            if self.tree['vulnerabilities'].get('notes') is None:
+                self.tree['vulnerabilities']['notes'] = []
+            self.hook = self.tree['vulnerabilities']['notes']
+
+    def always(self, root: RootType) -> None:
+        for data in root.Note:
+            category = data.attrib.get('Type', '').lower().replace(' ', '_')
+            record = {  # always
+                'text': data.text,
+                'category': category,
+            }
+            if category not in self.ENUM_CATEGORIES:
+                logging.error('Invalid document notes category %s. Should be one of: %s!', category, self.ENUM_MSG)
+                self.some_error = True
+            if audience := data.attrib.get('Audience'):  # sometimes
+                record['audience'] = audience
+            if title := data.attrib.get('Title'):  # sometimes
+                record['title'] = title
+            self.hook.append(record)
+
+    def sometimes(self, root: RootType) -> None:
+        pass
--- test/test_subtree.py
+++ test/test_subtree.py
@@ -83,40 +83,6 @@ HAS_TL_ACKS_XML = """\
    <!-- Document wide context information -->
    <DocumentTitle xml:lang="en">Acme Security Advisory for foo on bar - March 2017 - CSAF CVRF</DocumentTitle>
    <DocumentType xml:lang="en">Acme Security Advisory</DocumentType>
-   <DocumentPublisher Type="Vendor"/>
-   <DocumentTracking>
-      <Identification>
-         <ID>acme-2017-42</ID>
-      </Identification>
-      <Status>Final</Status>
-      <Version>1.0</Version>
-      <RevisionHistory>
-         <Revision>
-            <Number>1.0</Number>
-            <Date>2017-03-17T12:34:56-06:00</Date>
-            <Description>Initial Distribution</Description>
-         </Revision>
-         <Revision>
-            <Number>1.1</Number>
-            <Date>2017-03-18T01:23:45-06:00</Date>
-            <Description>Corrected Distribution</Description>
-         </Revision>
-      </RevisionHistory>
-      <InitialReleaseDate>2017-01-17T12:34:56-06:00</InitialReleaseDate>
-      <CurrentReleaseDate>2017-01-18T01:23:34-06:00</CurrentReleaseDate>
-   </DocumentTracking>
-   <DocumentNotes>
-      <Note Audience="All" Ordinal="1" Title="Summary" Type="Summary" xml:lang="en">
-         This document contains descriptions of Acme product security vulnerabilities with details on impacted ...
-         Additional information regarding ... be found at the Acme sites referenced in this document.</Note>
-   </DocumentNotes>
-   <DocumentDistribution>This document ... at: https://acme.example.com/sa/acme-2017-42-1-1.xml</DocumentDistribution>
-   <DocumentReferences>
-      <Reference Type="External">
-         <URL>https://acme.example.com/sa/acme-2017-42-1-1.json</URL>
-         <Description>URL to JSON version of Advisory</Description>
-      </Reference>
-   </DocumentReferences>
    <Acknowledgments>
       <Acknowledgment>
          <Name>Some One (not to be named explicitly)</Name>
@@ -134,81 +100,39 @@ HAS_TL_ACKS_XML = """\
          <URL>https://example.com/3</URL>
       </Acknowledgment>
    </Acknowledgments>
+</cvrfdoc>
+"""
 
-   <!-- Product tree section -->
-   <ProductTree xmlns="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/prod">
-      <Branch Name="Acme" Type="Vendor">
-         <Branch Name="Acme Things" Type="Product Family">
-            <Branch Name="Things On bar" Type="Product Name">
-               <Branch Name="1.9" Type="Product Version">
-                  <FullProductName ProductID="AC-FOO-1.9-on-bar">Foo 1.9 on bar</FullProductName>
-               </Branch>
-               <Branch Name="2.1" Type="Product Version">
-                  <FullProductName ProductID="AC-FOO-2.1-on-bar">Foo 2.1 on bar</FullProductName>
-               </Branch>
-            </Branch>
-            <Branch Name="Things On baz" Type="Product Name">
-               <Branch Name="1.9" Type="Product Version">
-                  <FullProductName ProductID="AC-FOO-1.9-on-baz">Foo 1.9 on baz</FullProductName>
-               </Branch>
-               <Branch Name="2.1" Type="Product Version">
-                  <FullProductName ProductID="AC-FOO-2.1-on-baz">Foo 2.1 on baz</FullProductName>
-               </Branch>
-            </Branch>
-         </Branch>
-       </Branch>
-   </ProductTree>
+HAS_TL_NOTES_XML = """\
+<?xml version='1.0'?>
+<cvrfdoc xmlns:cpe="http://cpe.mitre.org/language/2.0"
+   xmlns:cvrf="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/cvrf"
+   xmlns:cvrf-common="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/common"
+   xmlns:cvssv2="http://scap.nist.gov/schema/cvss-v2/1.0"
+   xmlns:cvssv3="https://www.first.org/cvss/cvss-v3.0.xsd"
+   xmlns:dc="http://purl.org/dc/elements/1.2/"
+   xmlns:ns0="http://purl.org/dc/elements/1.1/"
+   xmlns:prod="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/prod"
+   xmlns:scap-core="http://scap.nist.gov/schema/scap-core/1.0"
+   xmlns:sch="http://purl.oclc.org/dsdl/schematron"
+   xmlns:vuln="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/vuln"
+   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+   xmlns="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/cvrf">
 
-   <!-- Vulnerability sections -->
-   <Vulnerability Ordinal="1" xmlns="http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/vuln">
-      <Title>Vulnerability in the TCP component of Acme foo (CVE-2017-99999)</Title>
-      <Notes>
-         <Note Audience="All" Ordinal="1" Title="Details" Type="Details">
-            Vulnerability in the TCP component of Acme foo.
-            Supported versions that are affected are 1.9, and 2.0 when installed on bar but not affected when on baz.
-            Easily exploitable ... access via a single 0x42 value payload byte to compromise Acme foo.
-            Successful attacks of this vulnerability ... of service (DOS) of Acme foo.
-            CVSS 3.0 Base Score 9.8 (Confidentiality and Availability impacts).
-            CVSS Vector: CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H).</Note>
-      </Notes>
-      <Involvements>
-         <Involvement Party="Vendor" Status="Completed">
-            <Description>Fix has been released</Description>
-         </Involvement>
-      </Involvements>
-      <CVE>CVE-2017-99999</CVE>
-      <ProductStatuses>
-         <Status Type="Known Affected">
-            <ProductID>AC-FOO-1.9-on-bar</ProductID>
-            <ProductID>AC-FOO-2.1-on-bar</ProductID>
-         </Status>
-         <Status Type="Known Not Affected">
-            <ProductID>AC-FOO-1.9-on-baz</ProductID>
-            <ProductID>AC-FOO-2.1-on-baz</ProductID>
-         </Status>
-      </ProductStatuses>
-      <CVSSScoreSets>
-         <ScoreSetV3>
-            <BaseScoreV3>9.8</BaseScoreV3>
-            <VectorV3>CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H</VectorV3>
-         </ScoreSetV3>
-      </CVSSScoreSets>
-      <Remediations>
-         <Remediation Type="Vendor Fix">
-            <Description>acme-2017-42</Description>
-            <Entitlement xml:lang="it">Tutte le persone su questo pianeta</Entitlement>
-            <URL>https://acme.example.com/sa/acme-2017-42-1-1.html</URL>
-            <ProductID>AC-FOO-1.9-on-bar</ProductID>
-            <ProductID>AC-FOO-2.1-on-bar</ProductID>
-         </Remediation>
-      </Remediations>
-   </Vulnerability>
-   <!-- No more elements to follow -->
+   <!-- Document wide context information -->
+   <DocumentTitle xml:lang="en">Acme Security Advisory for foo on bar - March 2017 - CSAF CVRF</DocumentTitle>
+   <DocumentType xml:lang="en">Acme Security Advisory</DocumentType>
+   <DocumentNotes>
+      <Note Audience="All" Ordinal="1" Title="Summary" Type="Summary" xml:lang="en">
+         This document contains descriptions of Acme product security vulnerabilities with details on impacted ...
+         Additional information regarding ... be found at the Acme sites referenced in this document.</Note>
+   </DocumentNotes>
 </cvrfdoc>
 """
 
 ROOT_EXAMPLE_A = objectify.fromstring(EXAMPLE_A_XML)
 ROOT_HAS_TL_ACKS = objectify.fromstring(HAS_TL_ACKS_XML)
+ROOT_HAS_TL_NOTES = objectify.fromstring(HAS_TL_NOTES_XML)
 
 
 def test_document_leafs(caplog):
@@ -226,6 +150,7 @@ def test_document_leafs(caplog):
     assert 'ingesting sometimes present element' in caplog.text
 
 
+# raise OSError(lmxl_dump(ROOT_HAS_TL_ACKS))
 def lmxl_dump(el: Any) -> str:
     encoded: bytes = etree.tostring(el, encoding='utf-8', pretty_print=True, xml_declaration=True)
     return encoded.decode('utf-8')
@@ -267,6 +192,34 @@ def test_tl_acknowledgements(caplog):
     # raise OSError(lmxl_dump(ROOT_HAS_TL_ACKS))
     acks.load(ROOT_HAS_TL_ACKS.Acknowledgments)
     assert acks.dump() == expected
+    assert not caplog.text
+
+
+def test_tl_notes(caplog):
+    expected = {
+        'document': {
+            'notes': [
+                {
+                    'text': (
+                        '\n'
+                        '         This document contains descriptions of Acme product security vulnerabilities with'
+                        ' details on impacted ...\n'
+                        '         Additional information regarding ... be found at the Acme sites referenced in'
+                        ' this document.'
+                    ),
+                    'category': 'summary',
+                    'audience': 'All',
+                    'title': 'Summary',
+                }
+            ],
+        },
+    }
+
+    part = subtree.Notes(lc_parent_code='cvrf')
+    caplog.set_level(logging.ERROR)
+    part.load(ROOT_HAS_TL_NOTES.DocumentNotes)
+    assert not part.has_errors()
+    assert part.dump() == expected
     assert not caplog.text
 
 
@@ -315,15 +268,11 @@ def test_document_with_tl_acks(caplog):
     }
     acks = subtree.Acknowledgments(lc_parent_code='cvrf')
     caplog.set_level(logging.WARNING)
-    # raise OSError(lmxl_dump(ROOT_HAS_TL_ACKS))
     acks.load(ROOT_HAS_TL_ACKS.Acknowledgments)
     assert acks.dump() == expected_ack
     assert 'ingesting sometimes present element' in caplog.text
 
     expected_doc = {
-        'csaf_version': '2.0',
-        'category': 'Security Advisory',
-        'title': 'AppY Stream Control Transmission Protocol',
         'document': {
             'acknowledgments': [
                 {
@@ -350,6 +299,10 @@ def test_document_with_tl_acks(caplog):
                     ],
                 },
             ],
+            'category': 'Security Advisory',
+            'csaf_version': '2.0',
+            'title': 'AppY Stream Control Transmission Protocol',
         },
     }
-    doc = {**dle.dump(), **acks.dump()}
+    doc = {'document': dle.dump()['document'] | acks.dump()['document']}
+    assert doc == expected_doc
